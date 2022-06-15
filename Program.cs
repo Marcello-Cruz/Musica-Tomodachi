ed7b7a3be0a7ee6420b91cd1593ee39bf630e083
marcelloB2 <marcellinhopastelzinho@gmail.com>
2022-06-15 14:36:14 -0300

    add letras e cores

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.writeline ("Tomodachi ni wa narenai" foreground color = ConsoleColor.Red);
+Console.writeline ("Ser só seu amigo já não tá servindo mais" foreground color = ConsoleColor.Red);
+Console.writeline ("Entendo que é difícil, mas preciso me afastar" foreground color = ConsoleColor.Red);
+Console.writeline ("Tomō ga, daisuki" foreground color = ConsoleColor.Red);
+Console.writeline ("Tomodachi ni wa narenai" foreground color = ConsoleColor.Red);
+Console.writeline ("Ser só seu amigo já não tá servindo mais" foreground color = ConsoleColor.Red);
+Console.writeline ("Entendo que é difícil, mas preciso me afastar" foreground color = ConsoleColor.Red);
+Console.writeline ("Tomō ga, daisuki" foreground color = ConsoleColor.Red);
+
+console.readline();
+
+Console.writeline ("Mas 'cê tá do outro lado desse oceano " foreground color = ConsoleColor.Red);
+Console.writeline ("E eu tô tipo Rivers Cuomo, procurando como " foreground color = ConsoleColor.Red);
+Console.writeline ("Aprender japonês pra falar pr'aquela que eu amo " foreground color = ConsoleColor.Red);
+
+console.readline();
+
+Console.writeline ("Doshitara idesu ka? Tetsudatte itadakemasu ka? Yeah" foreground color = ConsoleColor.Red);
+Console.writeline ("Não vou mais chorar, tô vendo que 'cê nem vai ligar" foreground color = ConsoleColor.Red);
+Console.writeline ("Doshitara idesu ka? Tetsudatte itadakemasu ka? Yeah" foreground color = ConsoleColor.Red);
+Console.writeline ("Doshitara idesu ka? Doshitara idesu ka?" foreground color = ConsoleColor.Red);
+
+console.readline();
+
+Console.writeline ("Doshitara idesu ka? Why you're so cold?");
+Console.writeline ("Nani ga so nani damenano? Ore wa miteiru kimi no eyes");
+Console.writeline ("Oh, ge no hitomi ni nomikomareru, tsunami no naka maigo ni naru ore wo");
+Console.writeline ("Doko ni mo ikenai ore wa arukitsudzukeru, ye");
+Console.writeline ("Doshitara idesu ka? Why you're so cold?");
+Console.writeline ("Nani ga so nani damenano? Ore wa miteiru kimi no eyes");
+Console.writeline ("Oh, ge no hitomi ni nomikomareru, tsunami no naka maigo ni naru ore wo");
+Console.writeline ("Doko ni mo ikenai ore wa arukitsudzukeru");
+
+console.readline();
+
+Console.writeline ("Doshitara idesu ka? Tetsudatte itadakemasu ka? Yeah" foreground color = ConsoleColor.Red);
+Console.writeline ("Não vou mais chorar, tô vendo que 'cê nem vai ligar" foreground color = ConsoleColor.Red);
+Console.writeline ("Doshitara idesu ka? Tetsudatte itadakemasu ka? Yeah" foreground color = ConsoleColor.Red);
+Console.writeline ("Doshitara idesu ka? Doshitara idesu ka?" foreground color = ConsoleColor.Red);
